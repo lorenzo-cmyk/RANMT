37ee6b5d0cf992eb091eceee991e2d863b794062
lorenzo-cmyk <90748278+lorenzo-cmyk@users.noreply.github.com>
2026-04-12 16:41:53 +0200

    fix: update standalone CLI to support session_id param

--- src/rust/client/src/bin/client.rs
+++ src/rust/client/src/bin/client.rs
@@ -57,6 +57,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = ClientConfig {
         server_addr: cli.server,
         server_fqdn: None,
+        session_id: None,
         port: cli.port,
         direction,
         bitrate_bps: cli.bitrate,
